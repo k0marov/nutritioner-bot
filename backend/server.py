dfb342611c26964d5f02c803643a1f7578a3a969
drain678 <ekaterina1.zorina@gmail.com>
2024-05-22 20:48:52 +0300

    modification nutrition_provider in server.py

--- backend/server.py
+++ backend/server.py
@@ -2,42 +2,46 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import json
 from config import HEADER_TYPE, JSON_TYPE, HEADER_LENGTH
 from lib.datasources.providers import nutrition_fake
+from lib.service.interfaces.nutrition import NutritionProvider
 
 
-class NutritionerHandler(SimpleHTTPRequestHandler):
-    nutrition_provider = None
+def nutrition_handler_factory(nutrition_provider: NutritionProvider):
+    class NutritionerHandler(SimpleHTTPRequestHandler):
+        def do_POST(self):
+            if self.path != '/api/v1/meals':
+                self.send_response(404)
+                self.end_headers()
+                return
 
-    def do_POST(self):
-        if self.path != '/api/v1/meals':
-            self.send_response(404)
-            self.end_headers()
-            return
-    
-        content_length = int(self.headers[HEADER_LENGTH])
-        body = self.rfile.read(content_length)
-        info = json.loads(body)
+            content_length = int(self.headers[HEADER_LENGTH])
+            body = self.rfile.read(content_length)
+            info = json.loads(body)
 
-        if 'user_id' not in info or 'description' not in info:
-            self.send_response(400)
+            if 'user_id' not in info or 'description' not in info:
+                self.send_response(400)
+                self.send_header(HEADER_TYPE, JSON_TYPE)
+                self.end_headers()
+                response = {
+                    'error': 'Invalid request, missing user_id or description'}
+                self.wfile.write(json.dumps(response).encode('utf-8'))
+                return
+
+            user_id = info['user_id']
+            description = info['description']
+
+            self.send_response(200)
             self.send_header(HEADER_TYPE, JSON_TYPE)
             self.end_headers()
-            response = {'error': 'Invalid request, missing user_id or description'}
-            self.wfile.write(json.dumps(response).encode('utf-8'))
-            return
-            
-        user_id = info['user_id']
-        description = info['description']
-                    
-        self.send_response(200)
-        self.send_header(HEADER_TYPE, JSON_TYPE)
-        self.end_headers()
-        response = self.nutrition_provider.get_nutrition(
-            meal_description=description)
-        self.wfile.write(json.dumps({"calories": response.calories}).encode('utf-8'))
-        
+            response = nutrition_provider.get_nutrition(
+                meal_description=description)
+            self.wfile.write(json.dumps(
+                {"calories": response.calories}).encode('utf-8'))
 
-def run(nutrition_provider, server_class=HTTPServer, handler_class=NutritionerHandler, port=8000):
-    handler_class.nutrition_provider = nutrition_provider
+    return NutritionerHandler
+
+
+def run(nutrition_provider: NutritionProvider, server_class=HTTPServer, port=8000):
+    handler_class = nutrition_handler_factory(nutrition_provider)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f'Starting http server on port {port}...')
