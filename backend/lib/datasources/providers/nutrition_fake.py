2aa03a062aa1dcac5c5154af0a511acd50eeeec7
drain678 <ekaterina1.zorina@gmail.com>
2024-06-06 08:40:57 +0300

    fix by flake8 nutrition_fake.py

--- backend/lib/datasources/providers/nutrition_fake.py
+++ backend/lib/datasources/providers/nutrition_fake.py
@@ -1,12 +1,33 @@
+"""File with FakeNutritionProvider."""
+
+from lib import config
 from lib.service.interfaces import nutrition
 
 
 class FakeNutritionProvider(nutrition.NutritionProvider):
+    """A fake implementation of the NutritionProvider interface."""
+
     def __init__(self) -> None:
-        pass
+        """Initialize the FakeNutritionProvider instance."""
 
     def get_nutrition(self, meal_description: str) -> nutrition.NutritionInfo:
-        return nutrition.NutritionInfo(calories=500.0)
+        """Mock implementation of the get_nutrition method.
+
+        Args:
+            meal_description (str): The description of the meal.
+
+        Returns:
+            nutrition.NutritionInfo: A fake NutritionInfo object with calories set to 500.0.
+        """
+        return nutrition.NutritionInfo(calories=config.CAL500)
 
     def get_recommendations(self, past_data: list[nutrition.NutritionInfo]) -> str:
-        return f'надо меньше есть'
+        """Mock implementation of the get_recommendations method.
+
+        Args:
+            past_data (list[nutrition.NutritionInfo]): A list of past nutrition data.
+
+        Returns:
+            str: A fake recommendation message.
+        """
+        return 'надо меньше есть'
