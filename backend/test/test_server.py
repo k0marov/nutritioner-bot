a52563851bbbb6bc3cfa7d61a78c264dc5c69912
drain678 <ekaterina1.zorina@gmail.com>
2024-06-06 09:02:55 +0300

    fix by flake8 test_server.py

--- backend/test/test_server.py
+++ backend/test/test_server.py
@@ -1,16 +1,26 @@
-import pytest 
-import datetime
-import threading
-from lib.server import nutrition_handler_factory
-from lib.database.models import Meal
-from unittest import mock
-from lib.service.interfaces.nutrition import NutritionInfo 
-import http
-import unittest
-import requests
+"""File for testing the server."""
 
-class TestHTTPServer(unittest.TestCase):
+import datetime
+import http
+import threading
+from unittest import TestCase, mock
+
+import requests
+from lib import config
+from lib.database.models import Meal
+from lib.server import nutrition_handler_factory
+from lib.service.interfaces.nutrition import NutritionInfo
+
+
+class TestHTTPServer(TestCase):
+    """Class with tests for the server.
+
+    Args:
+        unittest (TestCase): Module for testing.
+    """
+
     def setUp(self):
+        """Set up a test environment before running tests."""
         self.repo_mock = mock.Mock()
         self.provider_mock = mock.Mock()
 
@@ -21,23 +31,25 @@ class TestHTTPServer(unittest.TestCase):
         self.thread.start()
 
     def tearDown(self):
+        """Tear down the test environment after running tests."""
         self.server.shutdown()
         self.thread.join()
-    
-    def _get_url(self) -> str: 
-        return f'http://localhost:{self.port}'
 
-    def test_POST(self): 
-        nutrition_info = NutritionInfo(calories=42)
+    def test_POST(self):
+        """Test the POST request for creating a meal."""
+        nutrition_info = NutritionInfo(calories=config.CAL42)
         self.provider_mock.get_nutrition = mock.MagicMock(return_value=nutrition_info)
         self.repo_mock.insert_meal = mock.MagicMock(return_value={'status': 'ok'})
         test_description = 'test meal description'
         user_id = '42'
-        response = requests.post(self._get_url()+'/api/v1/meals', json={
-            'user_id': user_id, 
-            'description': test_description
-        })
-        self.assertEqual(response.status_code, 200)
+        response = requests.post(
+            f'{self._get_url()}/api/v1/meals', json={
+                'user_id': user_id,
+                'description': test_description,
+            },
+            timeout=config.TIMEOUT,
+        )
+        self.assertEqual(response.status_code, config.OK)
         self.assertEqual(response.json()['calories'], nutrition_info.calories)
 
         self.provider_mock.get_nutrition.assert_called_once_with(meal_description=test_description)
@@ -47,26 +59,39 @@ class TestHTTPServer(unittest.TestCase):
         self.assertEqual(repo_call['calories'], nutrition_info.calories)
         self.assertEqual(repo_call['created_date'].date(), datetime.datetime.now().date())
 
-    def test_POST(self): 
+    def test_POST(self):
+        """Test the POST request for recommendations."""
         user_id = '42'
         now = datetime.datetime.now()
         saved_meals = [
-            Meal(calories=100, created_date=now), 
-            Meal(calories=200, created_date=now-datetime.timedelta(milliseconds=5)),
-            Meal(calories=400, created_date=now-datetime.timedelta(days=2)),
-            Meal(calories=1000, created_date=now-datetime.timedelta(days=4)),
+            Meal(calories=config.CAL100, created_date=now),
+            Meal(calories=config.CAL200, created_date=now-datetime.timedelta(milliseconds=5)),
+            Meal(calories=config.CAL400, created_date=now-datetime.timedelta(days=2)),
+            Meal(calories=config.CAL1000, created_date=now-datetime.timedelta(days=4)),
         ]
-        expected_flattened = [NutritionInfo(calories=300), None, NutritionInfo(calories=400), None, NutritionInfo(calories=1000), None, None]
+        expected_flattened = [
+            NutritionInfo(calories=config.CAL300), None, NutritionInfo(calories=config.CAL400),
+            None, NutritionInfo(calories=config.CAL1000), None, None,
+        ]
         self.repo_mock.get_meals_for_last_week = mock.MagicMock(return_value=saved_meals)
 
         test_recommendations = 'test recommendations'
         self.provider_mock.get_recommendations = mock.MagicMock(return_value=test_recommendations)
 
-        response = requests.get(self._get_url()+'/api/v1/stats', params={
-            'user_id': user_id,
-        })
-        self.assertEqual(response.status_code, 200)
+        response = requests.get(
+            f'{self._get_url()}/api/v1/stats', params={'user_id': user_id},
+            timeout=config.TIMEOUT,
+        )
+        self.assertEqual(response.status_code, config.OK)
         self.assertEqual(response.json()['recommendations'], test_recommendations)
 
         self.repo_mock.get_meals_for_last_week.assert_called_once_with(user_id)
         self.provider_mock.get_recommendations.assert_called_once_with(expected_flattened)
+
+    def _get_url(self) -> str:
+        """Get the URL for the test server.
+
+        Returns:
+            str: URL for the test server.
+        """
+        return f'http://localhost:{self.port}'
