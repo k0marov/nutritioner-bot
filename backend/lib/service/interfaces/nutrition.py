730b73ea0e506a4a21ecc6344f24873b0b75cb49
drain678 <ekaterina1.zorina@gmail.com>
2024-06-06 08:23:29 +0300

    fix by flake8 nutrition.py

--- backend/lib/service/interfaces/nutrition.py
+++ backend/lib/service/interfaces/nutrition.py
@@ -1,11 +1,27 @@
-from dataclasses import dataclass
+"""Class with interface of NutritionProvider and dataclass NutritionInfo."""
+
 import abc
+from dataclasses import dataclass
+
 
 @dataclass
-class NutritionInfo: 
+class NutritionInfo:
+    """Class to store nutritional information.
+
+    Attributes:
+        calories (float): The number of calories in the meal.
+    """
+
     calories: float
 
-class NutritionProvider(abc.ABC): 
+
+class NutritionProvider(abc.ABC):
+    """Abstract base class for a nutrition provider."""
+
     @abc.abstractmethod
     def get_nutrition(self, meal_description: str) -> NutritionInfo:
-        pass
+        """Get the nutritional information for a given meal description.
+
+        Args:
+            meal_description (str): The description of the meal.
+        """
